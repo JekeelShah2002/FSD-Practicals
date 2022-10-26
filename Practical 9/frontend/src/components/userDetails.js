ebd0a5322fb71cc5d7bd791e9c710ec3f7c9de93
Jekeel Shah <91594505+JekeelShah-20CS080@users.noreply.github.com>
2022-10-26 12:51:41 +0530

    Add files via upload

---
+++ Practical 9/frontend/src/components/userDetails.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+export default class UserDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userData: "",
+    };
+  }
+  componentDidMount() {
+    fetch("http://localhost:5000/userData", {
+      method: "POST",
+      crossDomain: true,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        token: window.localStorage.getItem("token"),
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, "userData");
+        this.setState({ userData: data.data });
+      });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Welcome to Homepage</h2>
+        Name<h2>{this.state.userData.fname} {this.state.userData.lname}</h2>
+        Email <h3>{this.state.userData.email}</h3>
+      </div>
+    );
+  }
+}
