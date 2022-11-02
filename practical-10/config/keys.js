4dc2d015e98cc82bec4451c5253d0b921a6f88e1
Jekeel Shah <91594505+JekeelShah-20CS080@users.noreply.github.com>
2022-11-02 16:17:53 +0530

    Add files via upload

---
+++ practical-10/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+	mongoURI:
+		"mongodb+srv://JekeelShah:<password>@cluster0.aymsivw.mongodb.net/?retryWrites=true&w=majority",
+	secretOrKey: "secret",
+};
---
+++ practical-10/server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
+
+const users = require("./routes/api/users");
+
+const app = express();
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+app.use(bodyParser.json());
+
+const dbURL = "mongodb://localhost:27017/mern-auth";
+
+//connect to MongoDB
+mongoose
+	.connect(
+		"mongodb+srv://Jekeel:jekukaju1269@cluster0.aymsivw.mongodb.net/?retryWrites=true&w=majority",
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useFindAndModify: false,
+			useCreateIndex: true,
+		}
+	)
+	.then(() => console.log("MongoDB successfully connected"))
+	.catch((err) => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server up and running on port ${port}`));
